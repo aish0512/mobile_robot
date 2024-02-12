c4caf4029cd86953f43e184bcb6fc0d8602925a6
Aishwarya Palit <aishwarya.palit01@gmail.com>
2024-02-12 16:15:47 +0530

    first comment

---
+++ build/mobile_nav/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/aishwarya/Desktop/mobile_robot/src/mobile_nav
+# Build directory: /home/aishwarya/Desktop/mobile_robot/build/mobile_nav
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
